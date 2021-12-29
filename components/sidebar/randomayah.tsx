13f427481f34aedb3c915d399643fb2e35f2d18f
azizoid <shaqhuseynov@gmail.com>
2021-12-29 02:07:11 +0100

    adding dynamic load

--- components/sidebar/randomayah.tsx
+++ components/sidebar/randomayah.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react"
 
-import Loader from "../loader.component"
-
 import SOORAH_LIST from "../../assets/soorahList"
-// import Skeleton from "react-loading-skeleton";
 
 const RandomAyah = (): JSX.Element => {
   const [out, setOut] = useState({
@@ -12,39 +9,27 @@ const RandomAyah = (): JSX.Element => {
     c: "Čitaj, u ime Gospodara tvoga, koji stvara,",
     t: 1,
   })
-  const [loaded, setLoaded] = useState(0)
-
-  const soorahList = SOORAH_LIST
 
   useEffect(() => {
     const fetchData = async () => {
       await fetch("https://mojkuran.com/api/random")
         .then((response) => response.json())
-        .then((data) => {
-          setOut(data.out)
-          setLoaded(1)
-        })
+        .then((data) => setOut(data.out))
     }
     fetchData()
   }, [])
 
-  if (!loaded) {
-    return <Loader />
-  }
-
   return (
     <div className="card">
       <div className="card-header">PROČITAJ JOŠ</div>
-      {loaded && (
-        <div className="card-body">
-          <h6 className="card-title text-muted">
-            {`${soorahList[out.s]}, ${out.a}`}
-          </h6>
-          <h6 className="card-title">
-            <a href={`/${out.s}/${out.a}?t=${out.t}`}>{out.c}</a>
-          </h6>
-        </div>
-      )}
+      <div className="card-body">
+        <h6 className="card-title text-muted">
+          {`${SOORAH_LIST[out.s]}, ${out.a}`}
+        </h6>
+        <h6 className="card-title">
+          <a href={`/${out.s}/${out.a}?t=${out.t}`}>{out.c}</a>
+        </h6>
+      </div>
     </div>
   )
 }
