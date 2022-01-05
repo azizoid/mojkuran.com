6ff8f392f9b693ad869f1254f17307d6ce13ab95
azizoid <shaqhuseynov@gmail.com>
2022-01-05 06:08:38 +0100

    adjust next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   swcMinify: true,
+  compress: false,
+  poweredByHeader: false,
+  generateEtags: false,
+  reactStrictMode: true,
+  devIndicators: {
+    buildActivity: false,
+  },
 }
