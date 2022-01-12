38eb715845dc16c6ccd94fe65810dac959b95046
azizoid <shaqhuseynov@gmail.com>
2022-01-12 06:37:21 +0100

    utility: add silently ignore on wrong request

--- utility/getApiData/getApiData.ts
+++ utility/getApiData/getApiData.ts
@@ -2,7 +2,8 @@ export const getApiData = async (url: string) =>
   await fetch(url)
     .then(response => {
       if (!response.ok) {
-        throw Error(response.statusText);
+        // throw Error(response.statusText);
+        // silently ignore
       }
       return response.json()
     }) //  workaround the catch 
