d0abe8c0134a9685a33c964e293f18700491ae49
azizoid <shaqhuseynov@gmail.com>
2022-01-12 05:03:01 +0100

    feat: use google tag manager plugin

--- components/sidebar/facebook.page.tsx
+++ components/sidebar/facebook.page.tsx
@@ -1,15 +1,5 @@
-import Script from "next/script"
-
 const FacebookPage = (): JSX.Element => (
   <div className="card d-none d-lg-block">
-    <div id="fb-root"></div>
-    <Script
-      async
-      defer
-      crossOrigin="anonymous"
-      src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0&appId=783652468370643&autoLogAppEvents=1"
-      nonce="YNCdRN8r"
-    />
     <div className="card-header">PRIDRUŹI NAM SE</div>
     <div className="col">
       <div
@@ -29,16 +19,6 @@ const FacebookPage = (): JSX.Element => (
           <a href="https://www.facebook.com/mojkuran/">Moj Kur&#039;an</a>
         </blockquote>
       </div>
-      {/* <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmojkuran&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
-        width="340"
-        height="130"
-        style={{ border: "none", overflow: "hidden" }}
-        scrolling="no"
-        frameBorder="0"
-        allowFullScreen={true}
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      ></iframe> */}
     </div>
   </div>
 )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useEffect } from "react"
 import type { AppProps } from "next/app"
-import Script from "next/script"
+import TagManager from "react-gtm-module"
 
 import NextNprogress from "nextjs-progressbar"
 
@@ -8,26 +8,17 @@ import { FormContextProvider } from "../store/form-store"
 
 import "../styles/app.scss"
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <FormContextProvider>
-    <Script
-      src="https://www.googletagmanager.com/gtag/js?id=UA-46216659-7"
-      strategy="afterInteractive"
-    />
-    <Script id="google-analytics" strategy="afterInteractive">
-      {`
-      window.dataLayer = window.dataLayer || []; 
-      function gtag() {window.dataLayer.push(arguments);}
-      gtag('js', new Date()); 
-      
-      gtag('config', 'UA-46216659-7');
-      `}
-    </Script>
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-NVN95DH" })
+  }, [])
+  return (
+    <FormContextProvider>
+      <NextNprogress />
 
-    <NextNprogress />
-
-    <Component {...pageProps} />
-  </FormContextProvider>
-)
+      <Component {...pageProps} />
+    </FormContextProvider>
+  )
+}
 
 export default MyApp
