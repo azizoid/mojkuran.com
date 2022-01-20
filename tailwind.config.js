c2be58bf0ccc29e5d692dd96317689999a1e948f
azizoid <shaqhuseynov@gmail.com>
2022-01-20 02:45:53 +0100

    chore: purge only jsx,tsx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layouts/*.{js,ts,jsx,tsx}",
-    "./ui/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{jsx,tsx}",
+    "./components/**/*.{jsx,tsx}",
+    "./layouts/*.{jsx,tsx}",
+    "./ui/**/*.{jsx,tsx}",
   ],
   theme: {
     extend: {},
