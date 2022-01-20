7271f2f5bdc869e2116dfcb8de0dfc436b5b3c32
azizoid <shaqhuseynov@gmail.com>
2022-01-20 01:38:00 +0100

    feat: create Card component

---
+++ ui/Card/Card.tsx
@@ -0,0 +1,31 @@
+import React, { FC, ReactNode } from "react"
+
+export type CardProps = {
+  title: string | ReactNode
+  titleClassName?: string | null
+  contentClass?: string | null
+  className?: string
+  size?: "small" | "medium"
+}
+
+enum cardSize {
+  small = "px-4",
+  medium = "px-7",
+}
+
+export const Card: FC<CardProps> = ({
+  title,
+  titleClassName,
+  className,
+  size = "medium",
+  children,
+}) => (
+  <div className="card">
+    <div className={`card-title ${cardSize[size]} ${titleClassName}`}>
+      {title}
+    </div>
+    <div className={`card-content ${cardSize[size]} ${className}`}>
+      {children}
+    </div>
+  </div>
+)
