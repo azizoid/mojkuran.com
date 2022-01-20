3fb5acc1a7eca454f8d26125a9e6cb9f3f610be0
azizoid <shaqhuseynov@gmail.com>
2022-01-20 01:38:15 +0100

    style: set Tailwind for Empty page

--- components/Empty/Empty.tsx
+++ components/Empty/Empty.tsx
@@ -1,72 +1,56 @@
 import Link from "next/link"
-import classnames from "classnames"
+import { Card } from "../../ui/Card/Card"
 
 export type EmptyProps = {
   alert: "danger" | "success"
 }
 
-export const Empty = ({ alert = "success" }): JSX.Element => (
-  <div className="row">
-    <div className="clearfix col">
-      <ol className="breadcrumb justify-content-center">
-        <li className="breadcrumb-item active">Čitaj:</li>
-        <li className="breadcrumb-item">
-          <Link href="/36">Sura Ja-sin</Link>
-        </li>
-        <li className="breadcrumb-item">
-          <Link href="/55">Sura Er-Rahman</Link>
-        </li>
-        <li className="breadcrumb-item">
-          <Link href="/67">Sura El-Mulk</Link>
-        </li>
-        <li className="breadcrumb-item">
-          <Link href="/2/255">Ajetul-Kursi</Link>
-        </li>
-      </ol>
+export const Empty = (): JSX.Element => (
+  <div className="space-y-4">
+    <ol className="py-2 w-full flex justify-center text-gray-500 space-x-5 md:text-sm">
+      <li className="">Čitaj:</li>
+      <li className="">
+        <Link href="/36">Sura Ja-sin</Link>
+      </li>
+      <li className="">
+        <Link href="/55">Sura Er-Rahman</Link>
+      </li>
+      <li className="">
+        <Link href="/67">Sura El-Mulk</Link>
+      </li>
+      <li className="">
+        <Link href="/2/255">Ajetul-Kursi</Link>
+      </li>
+    </ol>
+
+    <div className="col">
+      <h6 className="alert alert-success">
+        Kako biste imali <strong>uspješnu</strong> pretragu pratite slijedeća{" "}
+        <strong>pravila</strong>:
+      </h6>
     </div>
 
-    <div className="clearfix">
-      <div className="col">
-        <h6
-          className={classnames(
-            "text-start",
-            "alert",
-            alert === "danger" ? "alert-info" : "alert-success"
-          )}
-        >
-          Kako biste imali <strong>uspješnu</strong> pretragu pratite slijedeća{" "}
-          <strong>pravila</strong>:
-        </h6>
-      </div>
-
-      <table cellPadding="10">
-        <tbody>
-          <tr>
-            <td></td>
-            <td>
-              Uslovi korišćenja stranice: <code>mojkuran.com/96/1</code>
-            </td>
-          </tr>
-          <tr>
-            <td className="text-end align-top">Greške:</td>
-            <td>
-              Gramatičke greške vašeg unosa mogu biti razlog da ne dobijete
-              nikakav rezultat pretrage.
-              <br />
-              Na primjer: Umjesto <code>Mekka</code> napišite <code>Mekku</code>{" "}
-              ili <code>Mekke</code>
-            </td>
-          </tr>
-          <tr>
-            <td className="text-end align-top">Različite rečenice:</td>
-            <td>
-              Riječi i rečenice koje se možda ne nalaze u ovom prevodu. Na
-              primjer: U pretraživač NE unosite rečenice kao što su:{" "}
-              <code>&quot;Kako obaviti namaz&quot;</code>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="grid grid-cols-3 gap-2 py-4">
+      <Card
+        title="Uslovi korištenja"
+        className="flex flex-col space-y-4"
+        size="small"
+      >
+        <code>mojkuran.com/96/1</code>
+        <code>mojkuran.com/23</code>
+      </Card>
+      <Card title="Greške:" size="small">
+        Gramatičke greške vašeg unosa mogu biti razlog da ne dobijete nikakav
+        rezultat pretrage.
+        <br />
+        Na primjer: Umjesto <code>Mekka</code> napišete <code>Mekku</code> ili{" "}
+        <code>Mekke</code>
+      </Card>
+      <Card title="Različite rečenice:" size="small">
+        Riječi i rečenice koje se možda ne nalaze u ovom prevodu. Na primjer: U
+        pretraživač NE unosite rečenice kao što su:{" "}
+        <code>&quot;Kako obaviti namaz&quot;</code>
+      </Card>
     </div>
   </div>
 )
